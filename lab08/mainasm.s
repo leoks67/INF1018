99403f5d2bcf85ea0dd0f206f918a48ee0407d86
Leonardo Kac Szmajser <152468240+leoks67@users.noreply.github.com>
2026-04-07 16:54:05 +0000

    Update lab08

--- lab08/mainasm.s
+++ lab08/mainasm.s
@@ -1,20 +1,41 @@
 .data
-  nums: .int 3, -5, 7, 8, -2
-  s1:   .string "%d\n"
+nums: .int 3, -5, 7, 8, -2
+s1:   .string "%d\n"
 
-  .text
-  .globl main
-  main:
-  /* prologo */
-     pushq %rbp
-     movq  %rsp, %rbp
-     subq  $16, %rsp
-     movq  %rbx, -8(%rbp)
-     movq  %r12, -16(%rbp)
+.text
+.globl main
+main:
+/* prologo */
+  pushq %rbp
+  movq  %rsp, %rbp
+  subq  $16, %rsp
+  movq  %rbx, -8(%rbp)
+  movq  %r12, -16(%rbp)
 
-  /* coloque seu codigo aqui */
+/* coloque seu codigo aqui */
+  movl  $0, %ebx
+  movq  $nums, %r12
 
-  /* finalizacao */
+/* loop */
+L1:
+  cmpl  $5, %ebx
+  je    L2
+
+  movl  (%r12), %edi
+  movl  $1, %esi
+  call  filtro
+
+  movq  $s1, %rdi
+  movl  %eax, %esi
+  movl  $0, %eax
+  call  printf
+
+  addq  $4, %r12
+  addl  $1, %ebx
+  jmp   L1
+
+L2:
+/* finalizacao */
      movq -8(%rbp), %rbx
      movq -16(%rbp), %r12
      leave
